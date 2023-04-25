d8436e52e4cc70da438f5d96644a03bc1dbc60a3
Jesse Riggins <jriggins@users.noreply.github.com>
2023-04-25 08:48:07 -0500

    Initial

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,14 @@
+class HomeController < ApplicationController
+    def index
+        name = "Jesse"
+        content = <<~END
+<!DOCTYPE html><html lang="en"><meta charset="UTF-8">
+  <body>
+    <h1>Hello #{Name.all.sample.first_name}</h1>
+    <iframe src="https://giphy.com/embed/3pZipqyo1sqHDfJGtz" width="480" height="405" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sesamestreet-3pZipqyo1sqHDfJGtz">via GIPHY</a></p>
+  </body>
+<html>
+END
+        render html: content.html_safe
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Name.create(first_name: "Jesse")
+Name.create(first_name: "Ariel")
+Name.create(first_name: "Bill")
+Name.create(first_name: "Dahlia")
+Name.create(first_name: "Joe")
+Name.create(first_name: "John")
+Name.create(first_name: "Nick")
+Name.create(first_name: "Tim")
+Name.create(first_name: "Chris")
+Name.create(first_name: "Erica")
